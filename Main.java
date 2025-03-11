4950003ee7db23cc5ea781391abd1812f2361e40
Leonardo-F-Santana <discipuloleonardo@gmail.com>
2025-03-10 23:34:42 -0300

    Update syntaxe

--- Main.java
+++ Main.java
@@ -1,6 +1,31 @@
 public class Main {
     public static void Main(String [] args){
-        
-    }
+        //numeros inteiros
+        // byte => 8 bits => -128 a 127
+        // short => 16 bits => -32.768 a 32.767
+        // int => 32 bits => -2.147.483.648 a  2.147.483.647
+        // long =>64 bits => -9.223.372.036.854.775.808 a 9.223.372.036.854.775.807
+
+        // numeros decimais
+
+        // float => 32 bits => -3.40292347E+38 a +3.40292347E+38 precisao simples
+        // double => 64 bits => -1.79769313486231570E+308 a 1.79769313486231570E+308 precisao dupla
+
+        // String => palavras ou frases (aspas duplas)
+        // char => um unico caracter (aspas simples)
+        // boolean => true ou  false
+
+        byte b = 10;
+        short s = 100;
+        int i = 1000;
+        long l = 10000L;
+        float f = 20.0f;
+        double d = 200.0;
+        char c = 'L';
+        String s = "Leo";
+        boolean = true or false;
+
+
+    } 
     
 }
